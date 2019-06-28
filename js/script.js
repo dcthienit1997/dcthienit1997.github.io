0bfe47d28372fca2929800297381dd60b15fe39c
dcthienit1997 <dcthien.it.1997@gmail.com>
2019-06-28 16:02:55 +0700

    landing-page 2 DONE!

--- js/script.js
+++ js/script.js
@@ -7,3 +7,44 @@ $(window).on("load", function () {
     });
 
 });
+$(document).ready(function($) {
+
+    if($(window).width() > 1170) {
+        var headerHeight = $('.box-header').height();
+        console.log(headerHeight + "_headerHeight");
+        $(window).on("scroll", {previousTop: 0}, function() {
+            var currentTop = $(window).scrollTop();
+            console.log(currentTop + "_currentTop");
+            if (currentTop < this.previousTop) {
+                if (currentTop > 0 && $('.box-header').hasClass('is-fixed')) {
+                    $('.box-header').addClass('is-visible');
+                } else {
+                    $('.box-header').removeClass('is-visible is-fixed');
+                }
+            } else {
+                $('.box-header').removeClass('is-visible');
+                if (currentTop > headerHeight && !$('.box-header').hasClass('is-fixed')) {
+                    $('.box-header').addClass('is-fixed');
+                }
+            }
+            this.previousTop = currentTop;
+        })
+    }
+});
+
+// open/close primary navigation
+$('.box-primary-nav-trigger').on('click', function() {
+    $('.box-menu-icon').toggleClass('is-clicked');
+    $('.box-header').toggleClass('menu-is-open');
+    console.log('Click box-primary-nav-trigger');
+    if($('.box-primary-nav').hasClass('is-visible')) {
+        $('.box-primary-nav').removeClass('is-visible').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend',function(){
+            $('body').removeClass('overflow-hidden');
+        });
+    } else {
+        $('.box-primary-nav').addClass('is-visible').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend',function(){
+            $('body').addClass('overflow-hidden');
+        });	
+    }
+
+});
